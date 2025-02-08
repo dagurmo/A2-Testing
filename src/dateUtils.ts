fcdc5d943245ff5118e8d1eea328e92cf296b211
dagurmo <112622926+dagurmo@users.noreply.github.com>
2025-02-08 16:43:36 +0000

    switched moment with date-fns

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,23 +1,24 @@
-import moment from "moment";
+// import moment from "moment";
+import { add as addDateFns, isAfter, isBefore, isSameDay as isSameDayFns } from "date-fns";
 import { DATE_UNIT_TYPES } from "./constants";
 
 
 export function getCurrentYear(): number {
-  return moment().year();
+  return new Date().getFullYear();
 }
 
 export function add(date: Date, number: number, type = DATE_UNIT_TYPES.DAYS): Date {
-  return moment(date).add(number, type).toDate();
+  return addDateFns(date, { [type]: number });
 }
 
 export function isWithinRange(date: Date, from: Date, to: Date): boolean {
-  return moment(date).isBetween(from, to);
+  return isAfter(date, from) && isBefore(date, to);
 }
 
 export function isDateBefore(date: Date, compareDate: Date): boolean {
-  return moment(date).isBefore(compareDate);
+  return isBefore(date, compareDate);
 }
 
 export function isSameDay(date: Date, compareDate: Date): boolean {
-  return moment(date).isSame(compareDate);
+  return isSameDayFns(date, compareDate);
 }
